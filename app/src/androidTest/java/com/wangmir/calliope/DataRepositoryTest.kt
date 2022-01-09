3bf5d499887a19ba4c55ba221a8d22854348c952
Hyukjoong Kim <wangmir@gmail.com>
2022-01-09 23:26:52 +0900

    Implement data repository and its unit test with junit5.
    This commit contains,
    
    1. DataRepository Implementation using Android Room.
    2. Some adjustment for build.gradle to enable build, android junit5, support.
    3. Add Integration unittest case for data repository.
    
    Signed-off-by: Hyukjoong Kim <wangmir@gmail.com>

---
+++ app/src/androidTest/java/com/wangmir/calliope/DataRepositoryTest.kt
@@ -0,0 +1,175 @@
+package com.wangmir.calliope
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import com.wangmir.calliope.model.DataRepository
+import com.wangmir.calliope.model.adapters.data.DataRepositoryImpl
+import com.wangmir.calliope.model.adapters.data.LocalDatabase
+import com.wangmir.calliope.model.entities.Date
+import com.wangmir.calliope.model.entities.DayLog
+import com.wangmir.calliope.model.entities.EmotionLog
+import com.wangmir.calliope.model.entities.TextLog
+import com.wangmir.calliope.testutils.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.LocalDate
+import org.junit.jupiter.api.*
+
+@ExperimentalCoroutinesApi
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class DataRepositoryTest {
+
+    companion object {
+        private val context: Context = ApplicationProvider.getApplicationContext()
+        private val repository: DataRepository =
+            DataRepositoryImpl(LocalDatabase.getInMemoryDatabase(context).dayLogDao)
+    }
+
+    private val defaultDayLog
+        get() = DayLog(
+            Date(2021, 10, 21),
+            voiceLog = null,
+            textLog = TextLog("Test"),
+            sttLog = null,
+            EmotionLog.Happy
+        )
+
+    @Test
+    @Order(1)
+    fun testDayLogInsert() = runBlocking {
+        val dayLog = defaultDayLog
+
+        assertDoesNotThrow {
+            repository.insertDayLog(dayLog)
+        }
+    }
+
+    @Test
+    @Order(2)
+    fun testDayLogGet() = runBlocking {
+        val result = repository.getDayLog(LocalDate(2021, 10, 21))
+        assertThat(result.textLog.text).isEqualTo("Test")
+    }
+
+    @Test
+    @Order(3)
+    fun testDayLogDelete() = runBlocking {
+        val date = LocalDate(2021, 10, 21)
+        val beforeResult = repository.getDayLog(date)
+        assertThat(beforeResult).isNotNull()
+        repository.deleteDayLog(date)
+        val afterResult = repository.getDayLog(date)
+        assertThat(afterResult).isNull()
+    }
+
+    @Test
+    @Order(4)
+    fun testUpdateDayLog() = runBlocking {
+        val date = LocalDate(2021, 10, 21)
+        val dayLog = defaultDayLog
+
+        repository.insertDayLog(dayLog)
+        val beforeResult = repository.getDayLog(date)
+        assertThat(beforeResult.emotionLog).isEqualTo(EmotionLog.Happy)
+
+        val dayLogUpdate = dayLog.copy(emotionLog = EmotionLog.Sad)
+        repository.insertDayLog(dayLogUpdate)
+        val afterResult = repository.getDayLog(date)
+        assertThat(afterResult.emotionLog).isEqualTo(EmotionLog.Sad)
+    }
+
+    @Test
+    @Order(5)
+    fun testGetDayLogByYear() = runBlocking {
+
+        repository.insertDayLog(defaultDayLog)
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2021, 10, 22)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2021, 10, 23)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2021, 10, 24)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 22)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 23)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 24)))
+
+        var dayLogList = repository
+            .getDayLogsByYear(2021)
+            .getOrAwaitValue(this, dispatcher = Dispatchers.IO)
+
+        assertThat(dayLogList).isNotNull()
+        assertThat(dayLogList.size).isEqualTo(4)
+        assertThat(dayLogList::find{ it.date.dayOfMonth == 23 }!!.emotionLog).isEqualTo(EmotionLog.Happy)
+    }
+
+    @Test
+    @Order(6)
+    fun testGetDayLogByKeyword() = runBlocking {
+
+        repository.insertDayLog(defaultDayLog)
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 22),
+                textLog = TextLog("keyword handler")
+            )
+        )
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 23),
+                textLog = TextLog("keyword sword")
+            )
+        )
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 24),
+                textLog = TextLog("keyring sword")
+            )
+        )
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 22)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 23)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 24)))
+
+        var dayLogList = repository
+            .getDayLogsByKeyword("keyword")
+            .getOrAwaitValue(this, dispatcher = Dispatchers.IO)
+
+        assertThat(dayLogList).isNotNull()
+        assertThat(dayLogList.size).isEqualTo(2)
+        assertThat(dayLogList::find{ it.textLog.text == "keyword sword" }).isNotNull()
+    }
+
+    @Test
+    @Order(7)
+    fun testGetDayLogByEmotion() = runBlocking {
+
+        repository.insertDayLog(defaultDayLog)
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 22),
+                emotionLog = EmotionLog.Desirous
+            )
+        )
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 23),
+                emotionLog = EmotionLog.Joyful
+            )
+        )
+        repository.insertDayLog(
+            defaultDayLog.copy(
+                date = Date(2021, 10, 24),
+                emotionLog = EmotionLog.Joyful
+            )
+        )
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 22)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 23)))
+        repository.insertDayLog(defaultDayLog.copy(date = Date(2022, 10, 24)))
+
+        var dayLogList = repository
+            .getDayLogsByEmotion(EmotionLog.Joyful)
+            .getOrAwaitValue(this, dispatcher = Dispatchers.IO)
+
+        assertThat(dayLogList).isNotNull()
+        assertThat(dayLogList.size).isEqualTo(2)
+        assertThat(dayLogList.last().date.dayOfMonth).isEqualTo(24)
+    }
+}
---
+++ app/src/main/java/com/wangmir/calliope/model/adapters/data/BaseDao.kt
@@ -0,0 +1,82 @@
+package com.wangmir.calliope.model.adapters.data
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.RawQuery
+import androidx.room.Update
+import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.sqlite.db.SupportSQLiteQuery
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseDao<T> {
+    /**
+     * Insert an object in the database.
+     *
+     * @param obj the object to be inserted.
+     */
+    @Insert(onConflict = REPLACE)
+    abstract fun insert(obj: T)
+
+    /**
+     * Insert an array of objects in the database.
+     *
+     * @param obj the objects to be inserted.
+     */
+    @Insert(onConflict = REPLACE)
+    abstract fun insert(vararg obj: T)
+
+    /**
+     * Update an object from the database.
+     *
+     * @param obj the object to be updated
+     */
+    @Update
+    abstract fun update(obj: T)
+
+    /**
+     * Delete an object from the database
+     *
+     * @param obj the object to be deleted
+     */
+    @Delete
+    abstract fun delete(obj: T)
+
+
+    fun deleteAll(): Int {
+        val query = SimpleSQLiteQuery(
+            "delete from " + getTableName()
+        )
+        return doDeleteAll(query)
+    }
+
+    fun findAllValid(): List<T> {
+        val query = SimpleSQLiteQuery(
+            "select * from " + getTableName()
+        )
+        return doFindAllValid(query)
+    }
+
+    fun findById(id: Int): T {
+        val query = SimpleSQLiteQuery(
+            "select * from " + getTableName() + " where id = ?", arrayOf<Any>(id))
+        return doFind(query)
+    }
+
+    // NOTE: Room should not assign name of table explicitly to handle this.
+    private fun getTableName(): String {
+        val clazz = (javaClass.superclass.genericSuperclass as ParameterizedType)
+            .actualTypeArguments[0] as Class<*>
+        // tableName = StringUtil.toSnakeCase(clazz.getSimpleName());
+        return clazz.simpleName
+    }
+
+    @RawQuery
+    protected abstract fun doFindAllValid(query: SupportSQLiteQuery): List<T>
+
+    @RawQuery
+    protected abstract fun doFind(query: SupportSQLiteQuery): T
+
+    @RawQuery
+    protected abstract fun doDeleteAll(query: SupportSQLiteQuery): Int
+}
